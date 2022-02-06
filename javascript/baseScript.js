16a36474fdb8ece4bd34a0a76d5a04f230b9dd33
Goutai Phan <sb.phanquocthai@gmail.com>
2022-02-06 22:14:34 +0700

    commit

---
+++ javascript/baseScript.js
@@ -0,0 +1,68 @@
+export {inputArea, resultArea};
+import {input, clear, clearAll, result} from './functionScript.js';
+
+let inputArray = [['AC', 'C', '÷', 7, 8, 9],
+    ['(', ')', '×', 4, 5, 6],
+    ['%', '‰', '-', 1, 2, 3],
+    ['√', '^', '+', '.', 0, '=']];
+let inputArea = document.getElementById('inputArea');
+let resultArea = document.getElementById('resultArea');
+let tableArea = document.getElementById('tableArea');
+
+for (let i = 0; i < inputArray.length; i++) {
+    let tableRow = document.createElement('tr');
+    tableArea.appendChild(tableRow);
+
+    for (let j = 0; j < inputArray[i].length; j++) {
+        let button = document.createElement('button');
+        button.innerHTML = String(inputArray[i][j]);
+        if (button.innerHTML.match(/[0-9]/)) {
+            button.className = 'buttonNumber';
+        } else {
+            button.className = 'buttonCalculus';
+        }
+
+        let tableCell = document.createElement('td');
+        tableCell.appendChild(button);
+        tableRow.appendChild(tableCell);
+
+        switch (button.innerHTML) {
+            default:
+                button.onclick = function () {
+                    input(button.innerHTML);
+                };
+                break;
+            case 'C':
+                button.onclick = clear;
+                break;
+            case 'AC':
+                button.onclick = clearAll;
+                break;
+            case '=':
+                button.onclick = result;
+                break;
+        }
+    }
+}
+
+window.onkeydown = function (event) {
+    inputArea.setCustomValidity('');
+    event.preventDefault();
+    switch (event.key) {
+        case String(event.key.match(/[0-9.,+\-*/%()^√]/)):
+            input(event.key);
+            break;
+        case 'Backspace':
+            clear();
+            break;
+        case 'Delete':
+        case 'Clear':
+            clearAll();
+            break;
+        case 'Enter':
+        case 'Return':
+            result();
+            break;
+    }
+}
+
---
+++ javascript/functionScript.js
@@ -0,0 +1,67 @@
+export {input, clear, clearAll, result};
+import {inputArea, resultArea} from './baseScript.js';
+
+function input(key) {
+    inputArea.value += key.replace('×', '*')
+        .replace('÷', '/')
+        .replace('√', '√(')
+        .replace('^', '^(');
+    resultArea.innerHTML = '';
+}
+
+function clear() {
+    inputArea.value = inputArea.value.slice(0, -1);
+    resultArea.innerHTML = '';
+}
+
+function clearAll() {
+    inputArea.value = '';
+    resultArea.innerHTML = '';
+}
+
+function result() {
+    let formatValue = format(inputArea.value);
+    inputArea.value = formatValue.replaceAll('Math.sqrt(', '√\(');
+
+    try {
+        let result = eval(formatValue);
+        if (formatValue.includes('^')) {
+            let caretAmount = (formatValue.match(/\^/g)).length;
+            let powers = formatValue.split('^');
+            if (caretAmount >= 2) {
+                inputArea.setCustomValidity('Biểu thức chỉ thực hiện 1 phép lũy thừa.');
+                inputArea.reportValidity();
+            } else {
+                resultArea.innerHTML = String(Math.pow(eval(powers[0]), eval(powers[1])));
+            }
+        } else if (typeof (result) === 'number') {
+            resultArea.innerHTML = String(result);
+        } else {
+            inputArea.setCustomValidity('Biểu thức không hợp lệ.');
+            inputArea.reportValidity();
+        }
+    } catch {
+        inputArea.setCustomValidity('Biểu thức không hợp lệ.');
+        inputArea.reportValidity();
+    }
+}
+
+function format(inputValue) {
+    let reMinus = inputValue.match(/(-)\1+/g);
+    if (reMinus !== null) {
+        for (let i = 0; i < reMinus.length; i++) {
+            inputValue = reMinus[i].length % 2 === 0
+                ? inputValue.replace(reMinus[i], '+')
+                : inputValue.replace(reMinus[i], '-');
+        }
+    }
+    return inputValue.replaceAll('%', '/100')
+        .replaceAll('‰', '/1000')
+        .replaceAll('√\(', 'Math.sqrt(')
+        .replaceAll(',', '.')
+        .replace(/(\+)\1+/g, '+')
+        .replace(/(\*)\1+/g, '*')
+        .replace(/(\/)\1+/g, '/')
+        .replace(/(\.)\1+/g, '.');
+}
+
