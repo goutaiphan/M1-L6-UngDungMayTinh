014e83802bc95497f3a5563daf41da903389681f
Goutai Phan <sb.phanquocthai@gmail.com>
2022-02-11 02:36:41 +0700

    commit

--- javascript/baseScript.js
+++ javascript/baseScript.js
@@ -11,7 +11,7 @@ alert(width);
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     document.body.style.transform = `scale(${widthRatio})`;
-    
+    document.body.style.width = width + 'px';
     document.body.style.paddingBottom = 200 + 'px';
 } else {
     document.body.style.width = '450px';
