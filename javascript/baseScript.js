804ac6d5966798795bb8d3bbb9f0aa27cdd784f5
Goutai Phan <sb.phanquocthai@gmail.com>
2022-02-11 04:02:43 +0700

    commit

--- javascript/baseScript.js
+++ javascript/baseScript.js
@@ -8,14 +8,13 @@ let widthRatio = width / 500;
 if (screen.width <= 320) {
     widthRatio = widthRatio * 1.3;
     document.body.style.width = width + 'px';
-    document.body.style.height = innerHeight + 'px';
+    document.body.style.height = height + 'px';
     document.body.style.paddingTop = 50 + 'px';
     document.body.style.paddingBottom = 100 + 'px';
-} else if (screen.width < 400) {
+} else if (screen.width <= 400) {
     widthRatio = widthRatio * 1.1;
     document.body.style.width = width + 'px';
     document.body.style.height = innerHeight + 'px';
-    document.body.style.paddingTop = 50 + 'px';
     document.body.style.paddingBottom = 100 + 'px';
 } else if (screen.width < 768) {
     document.body.style.width = width + 'px';
